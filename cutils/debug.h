90034b2cb64f663a22ed94d8a5ed83ef2ff33d00
damnever <dxc.wolf@gmail.com>
2016-06-29 23:46:16 +0800

    debug

--- cutils/debug.h
+++ cutils/debug.h
@@ -1,13 +1,20 @@
-#ifndef _TEST_H_
-#define _TEST_H_
+#ifndef __DBG_H__
+#define __DBG_H__
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 
-#define DEBUG(format, args...)                          \
-    do {                                                \
-        fprintf(stderr, "DEBUG: %s:%d:%s(): "#format,   \
-                __FILE__, __LINE__, __func__,  ##args); \
-    } while (0);
+#ifdef NDEBUG
+#define debug(FMT, ...)
+#else
+#define debug(FMT, ...) fprintf(stderr, "DEBUG %s:%s:%d: " FMT "\n", __FILE__, __func__, __LINE__, ##__VA_ARGS__)
+#endif
+
+#define str_errno() (errno == 0 ? "None" : strerror(errno))
+
+#define log_err(FMT, ...) fprintf(stderr, "[ERROR] (%s:%s:%d errno: %s) " FMT "\n", __FILE__, __func__, __LINE__, str_errno(), ##__VA_ARGS__)
+#define log_warn(FMT, ...) fprintf(stderr, "[WARN] (%s:%s:%d errno: %s) " FMT "\n", __FILE__, __func__, __LINE__, str_errno(), ##__VA_ARGS__)
+#define log_info(FMT, ...) fprintf(stderr, "[INFO] (%s:%s:%d) " FMT "\n", __FILE__, __func__, __LINE__, ##__VA_ARGS__)
 
 #endif
