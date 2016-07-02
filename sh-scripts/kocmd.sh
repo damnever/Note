473a425fb868acbebc8b3faff37ed9118585a868
damnever <dxc.wolf@gmail.com>
2016-07-03 01:13:00 +0800

    kill a bunch of process..

--- sh-scripts/kocmd.sh
+++ sh-scripts/kocmd.sh
@@ -11,5 +11,5 @@ kocmd() {
         fi
         ((++index))
     done
-    kill -9 $(ps -aux | grep "$name" | awk '{print $2}' | head -n 1)
+    kill -9 $(ps -aux | grep "$name" | grep -v "grep" | awk '{print $2}')
 }
