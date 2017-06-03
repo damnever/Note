5cacc094008d6f9bcf9f6d80216cf71bd412df08
damnever <dxc.wolf@gmail.com>
2017-06-03 21:58:12 +0800

    Another version

--- algorithms/detect_capital.go
+++ algorithms/detect_capital.go
@@ -4,6 +4,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func detectCapitalUse(word string) bool {
@@ -15,10 +16,26 @@ func detectCapitalUse(word string) bool {
 			icap = i
 			ncap++
 		}
-		last = i // faster than len
+		last = i
 	}
 	if (ncap > 1 && ncap < last+1) || (ncap == 1 && icap != 0) {
 		return false
+	}
+	return true
+}
+
+func detectCapitalUse2(word string) bool {
+	ncap := 0
+	for i, c := range word {
+		n := i + 1
+		if c >= 'A' && c <= 'Z' {
+			ncap++
+			if (ncap != n) && i != 0 {
+				return false
+			}
+		} else if ncap > 1 && ncap < n {
+			return false
+		}
 	}
 	return true
 }
@@ -29,4 +46,28 @@ func main() {
 	fmt.Printf("Google = %v\n", detectCapitalUse("Google"))
 	fmt.Printf("FlaG = %v\n", detectCapitalUse("FlaG"))
 	fmt.Printf("flaG = %v\n", detectCapitalUse("flaG"))
+	fmt.Printf("FLag = %v\n", detectCapitalUse("FLag"))
+
+	start := time.Now()
+	for i := 0; i < 1e7; i++ {
+		detectCapitalUse("USA")
+		detectCapitalUse("leetcode")
+		detectCapitalUse("Google")
+		detectCapitalUse("FlaG")
+		detectCapitalUse("flaG")
+		detectCapitalUse("FLag")
+	}
+	fmt.Println(time.Since(start).Nanoseconds() / 1e7)
+
+	start = time.Now()
+	for i := 0; i < 1e7; i++ {
+		detectCapitalUse2("USA")
+		detectCapitalUse2("leetcode")
+		detectCapitalUse2("Google")
+		detectCapitalUse2("FlaG")
+		detectCapitalUse2("flaG")
+		detectCapitalUse2("FLag")
+	}
+	fmt.Println(time.Since(start).Nanoseconds() / 1e7)
+
 }
